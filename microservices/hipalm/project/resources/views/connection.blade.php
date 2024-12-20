c5b17643ec054dc70c90bcb649ef979694a06d2a
MicheleGenchi <cell.mgenchi@gmail.com>
2024-12-20 22:30:09 +0100

    Creata una pagina di test per la connessione

--- microservices/hipalm/project/resources/views/connection.blade.php
+++ microservices/hipalm/project/resources/views/connection.blade.php
@@ -1,17 +1,30 @@
 <?php
-$dsn = 'mysql:dbname=hipalm;host=db';
-$user = 'hipalm';
-$password = 'root';
+
+$dsn = $_POST["dsn"] ?? 'mysql:dbname=hipalm;host=db';
+$user = $_POST['user'] ?? 'hipalm';
+$password = $_POST["password"] ?? 'root';
 
 try {
 	$dbh = new PDO($dsn, $user, $password);
-	return ["Connection" => "OK",
-			[data =>
-			["dsn" => $dsn,
-			"user"=> $user,
-			"password" => "******"];
+	$response = [
+		"Connection" => "OK",
+		"data" =>
+			[
+				"dsn" => $dsn,
+				"user" => $user,
+				"password" => "******"
 			]
-];
+
+	];
+	echo "<p>{"."</p><br/>";
+	echo "&nbsp;"."Connection => ".$response["Connection"];
+	echo "&emsp;"."data => { ";
+	echo "&emsp;&emsp;"."dsn => ".$response["data"]["dsn"];
+	echo "&emsp;&emsp;"."user => ".$response["data"]["user"];
+	echo "&emsp;&emsp;"."password => ".$response["data"]["password"];
+	echo "&emsp;"."}";
+	echo "&nbsp;}</p>";
+
 } catch (PDOException $e) {
 	echo 'Connection failed: ' . $e->getMessage();
 }
