acbabec103b9b6f8a958c258554076cb3e92d858
MicheleGenchi <cell.mgenchi@gmail.com>
2024-12-21 15:52:17 +0100

    Migliora la grafica

--- microservices/hipalm/project/resources/views/connection.blade.php
+++ microservices/hipalm/project/resources/views/connection.blade.php
@@ -1,30 +1,26 @@
 <?php
-
-$dsn = $_POST["dsn"] ?? 'mysql:dbname=hipalm;host=db';
-$user = $_POST['user'] ?? 'hipalm';
-$password = $_POST["password"] ?? 'root';
+	$dsn = $_POST["dsn"] ?? 'mysql:dbname=hipalm;host=db';
+	$user = $_POST['user'] ?? 'hipalm';
+	$password = $_POST["password"] ?? 'root';
 
 try {
-	$dbh = new PDO($dsn, $user, $password);
-	$response = [
-		"Connection" => "OK",
-		"data" =>
-			[
-				"dsn" => $dsn,
-				"user" => $user,
-				"password" => "******"
-			]
-
-	];
-	echo "<p>{";
-	echo "<br>&nbsp;"."Connection => ".$response["Connection"];
-	echo "<br>&emsp;"."data => { ";
-	echo "<br>&emsp;&emsp;"."dsn => ".$response["data"]["dsn"];
-	echo "<br>&emsp;&emsp;"."user => ".$response["data"]["user"];
-	echo "<br>&emsp;&emsp;"."password => ".$response["data"]["password"];
-	echo "<br>&emsp;"."}";
-	echo "<br>&nbsp;}</p>";
-
+	$db = new PDO($dsn, $user, $password);
+	$tables = $db->query("SHOW TABLES");
+	echo "<h5>";
+	echo "&nbsp;"."Connection => OK";
+	echo "<br>&emsp;&emsp;"."dsn &emsp;&emsp;&nbsp;&nbsp;=> $dsn";
+	echo "<br>&emsp;&emsp;"."user &emsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;=> $user";
+	echo "<br>&emsp;&emsp;"."password => $password";
+	echo "</h5>";
+	echo "<hr>";
+	echo "<h3>Tabelle</h3>";	
+	$query=$db->query("SHOW TABLES");
+	$tables=$query->fetchAll(PDO::FETCH_COLUMN);
+	echo "<h4>";
+	foreach ($tables as $i => $value) {
+		echo $i." => ".$value."<br>";
+	}
+	echo "</h4>";
 } catch (PDOException $e) {
 	echo 'Connection failed: ' . $e->getMessage();
 }
