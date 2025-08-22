a666874a58f91aa7259f1baceff82d01e2c1f8ca
MicheleGenchi <cell.mgenchi@gmail.com>
2025-08-22 11:59:23 +0200

    Cancella hipalm

--- microservices/hipalm/project/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Controller;
-
-Route::View('/', 'welcome');
-Route::Get('testconnessione', [Controller::class, 'testconnection']);
-Route::View('connessione', 'connessione');
