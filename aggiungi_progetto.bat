cb90fab9f1353531bcee1ee2991c32e7d5d5ee28
MicheleGenchi <cell.mgenchi@gmail.com>
2025-09-13 23:41:04 +0200

    zip class solzuione

--- aggiungi_progetto.bat
+++ aggiungi_progetto.bat
@@ -22,8 +22,8 @@ echo RUN pecl install xdebug
 echo RUN docker-php-ext-enable xdebug
 echo RUN curl -sS https://getcomposer.org/installer ^| php -- --install-dir=/usr/local/bin --filename=composer
 echo RUN apt update -y \
-echo        ^&^& apt install git zip zsh -y 
-echo RUN docker-php-ext-install pdo pdo_mysql
+echo        ^&^& apt install git libzip-dev zsh -y 
+echo RUN docker-php-ext-install pdo pdo_mysql zip
 echo RUN sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)"
 echo WORKDIR /%NOMEPROGETTO%
 )>Dockerfile
